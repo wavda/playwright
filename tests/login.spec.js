efec8511141cfd4fe8a6cb0bb5e1f162cf0aeff5
Wavda Aniva Zabidi <wavda.zabidi@hostinger.com>
2022-07-11 19:17:54 +0700

    improve report

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,7 @@
 const { test, expect, devices } = require('@playwright/test');
 
 const config = {
-  reporter: [ ['html', { open: 'always' }] ],
+  reporter: [ ['junit', { outputFile: 'results.xml' }] ],
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   use: {
@@ -30,5 +30,5 @@ test('Login as admin', async ({ page }) => {
   await page.locator('input[name="txtPassword"]').fill('admin123');
   await page.locator('input:has-text("LOGIN")').click();
   await page.waitForURL('https://opensource-demo.orangehrmlive.com/index.php/dashboard');
-  await page.locator('text=Welcome Saurabh').click();
+  await page.locator('id=welcome').click();
 });
